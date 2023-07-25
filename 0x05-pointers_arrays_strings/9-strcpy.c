301babb4de1c5fab054bf533c1ee738ac93356a6
kbay9 <kobenaenyam@gmail.com>
2023-07-25 11:14:14 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdio.h>
+#include "main.h"
+#include <stdio.h>
 /**
  * _strcpy - copies the string pointed to by src,
  * including the terminating null byte, to the
@@ -12,12 +14,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int count = 0;
 
-        while (count >= 0)
+	while (count >= 0)
 	{
-	*(dest+ count) = *(src + count);
-	if (*(src + count)== '\0')
-		break;
-	count ++;
+		*(dest + count) = *(src + count);
+		if (*(src + count) == '\0')
+			break;
+		count++;
 	}
 	return (dest);
 }
