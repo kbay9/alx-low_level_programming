caab40e67235281f55e856e42e04eb3bc3e07a2d
kbay9 <kobenaenyam@gmail.com>
2023-09-05 03:45:16 -0700

    done again

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,15 +2,15 @@
 
 /**
  * read_textfile - reads a text file and prints the letters
- * @filename: filename
+ * @filename: filename.
  * @letters: numbers of letters printed.
  *
  * Return: numbers of letters printed. It fails, returns 0.
  */
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nrd,nwr;
+	ssize_t nrd, nwr;
 	char *buf;
 
 	if (!filename)
@@ -18,15 +18,15 @@ ssize_t read_textfile(const char *filename, size_t letters);
 
 	fd = open(filename, O_RDONLY);
 
-	if(fd == -1)
-		return(0);
+	if (fd == -1)
+		return (0);
 
-	buf = malloc(sizeof(char) *(letters));
+	buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
 
-	nrd = read(fd,buf,letters);
-	nwr = write(STDOUT_FILENO,buf,nrd);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
 
 	close(fd);
 
